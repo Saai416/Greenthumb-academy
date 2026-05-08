bd3bbccd6d5a86cd759bc79bf9b05b15b7ca3a6c
Saai416 <Saiprakaash48@gmail.com>
2026-05-08 09:11:15 +0530

    updated

--- src/pages/sections/ContactSection.tsx
+++ src/pages/sections/ContactSection.tsx
@@ -150,7 +150,7 @@ export function ContactSection() {
             {/* Google Maps embed */}
             <div className="overflow-hidden rounded-2xl shadow-soft border border-border">
               <iframe
-                src="https://www.google.com/maps?q=12.929821,80.215268&output=embed"
+                src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3888.655942970907!2d80.21269307507578!3d12.929820987381776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTLCsDU1JzQ3LjQiTiA4MMKwMTInNTUuMCJF!5e0!3m2!1sen!2sin!4v1778211540642!5m2!1sen!2sin"
                 width="100%"
                 height="400"
                 style={{ border: 0, display: "block" }}
